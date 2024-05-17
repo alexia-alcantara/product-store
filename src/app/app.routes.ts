9fbeb8ea1aa60f9711bae83296b4a4b2c34011b8
Alexia <alexialcantara@outlook.com>
2024-05-17 12:09:58 -0300

    feat: adiciona componente de listagem

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ListComponent } from './features/list/list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '', component: ListComponent
+}];
