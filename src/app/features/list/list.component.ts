8741adccc266cbcb4ef24475613e499c49fd8e6d
Alexia <alexialcantara@outlook.com>
2024-07-02 14:46:52 -0300

    feat: adiciona Card para mostrar produtos

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,12 +1,17 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interface/product.interface';
 
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from "@angular/material/button"
+
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatButtonModule
+  ],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
