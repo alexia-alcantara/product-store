eca7068fa22ed4836a7ce388bdaf6779ead54540
Alexia <alexialcantara@outlook.com>
2024-07-02 15:16:04 -0300

    feat: adiciona link para criar um produto

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -2,12 +2,16 @@ import { Component, inject } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interface/product.interface';
 import { CardComponent } from './components/card/card.component';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-list',
   standalone: true,
   imports: [
-    CardComponent
+    CardComponent,
+    RouterLink,
+    MatButtonModule
   ],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
